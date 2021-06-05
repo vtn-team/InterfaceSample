356e1a7ae113c5e26feec61c84bd7567409c6258
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-06-05 17:11:53 +0900

    Mustの内容を完成させた

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : FieldObject
+{
+    // Start is called before the first frame update
+    public override void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+
+    public override void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
