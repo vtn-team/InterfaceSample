23878b7226cf5a58138c26aba77aa7edf6061ec1
コウザイ2000 <a07036142122@gmail.com>
2021-06-05 14:40:35 +0900

    Mustだけやった

---
+++ Assets/Scripts/Kill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour, ISave
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject, 0.2f);
+        }
+    }
+    public virtual void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+
+    public virtual void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+}
