cc28e373cf6c00f8d2b186b3d2e236466a6d7a38
yosho_cho <choyosho165152987@gmail.com>
2021-06-05 18:02:52 +0900

    tryまでやりました

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,20 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : FieldObject, ISave
+public class Enemy : FieldObject
 {
-
     public override void Save(SaveData.ObjectData data)
     {
         base.Save(data);
     }
-
-    public override void Load(SaveData.ObjectData data)
-    {
-        base.Load(data);
-    }
-
-   
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
