6c6320e33adfe4a7ac2fc5f42314c98f55d38fae
LRcatR <v_t3ixipmi@outlook.jp>
2021-06-05 14:31:52 +0900

    Tryまでやった

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : TestA, ISave 
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    public override void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+}
