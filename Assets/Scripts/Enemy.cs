bd3bc8f3c509838c2e6c936a5356a55cec1a2286
yosho_cho <choyosho165152987@gmail.com>
2021-06-05 17:36:03 +0900

    Mustの課題を終わらせました

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : FieldObject, ISave
+{
+
+    public override void Save(SaveData.ObjectData data)
+    {
+        base.Save(data);
+    }
+
+    public override void Load(SaveData.ObjectData data)
+    {
+        base.Load(data);
+    }
+
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("HIT");
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
