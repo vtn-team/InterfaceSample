0377907ca9d4c23f6562ac279a0aaa96ca8271c9
k.mitarai <kazmit299^gmail.com>
2021-06-05 10:03:19 +0900

    設定不要にする

--- Assets/Scripts/FieldObject.cs
+++ Assets/Scripts/FieldObject.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class FieldObject : MonoBehaviour, ISave
 {
-    [SerializeField] string _prefabName = "";
-
     public virtual void Save(SaveData.ObjectData data)
     {
-        data.PrefabName = _prefabName;
+        data.PrefabName = this.name.Replace("(Clone)", "");
         data.Position = this.transform.position;
     }
 
