38720e6001f6194131a6d8be9905b72c2221508a
nogu06 <knocktokotoko@gmail.com>
2021-06-05 17:03:43 +0900

    Tryができた

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ISave
+{
+    public void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+
+    public void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Player";
+        data.Position = this.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.gameObject);
+            Destroy(this);
+        }
+    }
+
+}
