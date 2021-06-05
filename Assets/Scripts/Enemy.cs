76ab8ad3f4e547e1149ee5a9c512e53529b2ab3f
DESKTOP-9S7UCKQ\Yuno <yuno0923.fran@ezweb.ne.jp>
2021-06-05 15:11:37 +0900

    再コミット

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour,ISave
+{
+    // Start is called before the first frame update
+    public virtual void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    public virtual void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
