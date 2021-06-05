109ea3ceb0ad1d6571150b15e556fd48e4071696
UCGht <mainkurahutogasuto@gmail.com>
2021-06-05 15:11:05 +0900

    Tryまで終了

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : loaddata
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject,0.5f);
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, ISave
+public class Player : loaddata
 {
-    public virtual void Save(SaveData.ObjectData data)
-    {
-        data.PrefabName = "Player";
-        data.Position = this.transform.position;
-    }
 
-    public virtual void Load(SaveData.ObjectData data)
-    {
-        this.transform.position = data.Position;
-    }
 }
