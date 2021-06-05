fc6d56ca31338a4438f40b72a5e6d0fe9a888060
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-06-05 18:20:21 +0900

    課題のMustの回答を作成した。

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : FieldObject
+public class Enemy : FieldObject
 {
+    //課題:Try
     public override void Save(SaveData.ObjectData data)
     {
-        data.PrefabName = "Player";
+        data.PrefabName = "Enemy";
         data.Position = this.transform.position;
     }
-
     public override void Load(SaveData.ObjectData data)
     {
         this.transform.position = data.Position;
+    }
+
+    //課題:Must
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
