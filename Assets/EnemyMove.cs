be9c82bc93c4cd53f6018b3000cf267ea23eeaa8
DESKTOP-RDAJ7SC\nixka <nixkairi0219@gmail.com>
2021-06-05 15:06:24 +0900

    データを追加

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private GameObject[] targets; // 目的地
+    private int nextIndex = 0; //次の目的地のインデックス
+    private UnityEngine.AI.NavMeshAgent agent; //自動で動くオブジェクト
+
+    void Start()
+    {
+        // アタッチされているオブジェクトのNaveMeshAgentを取得
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        // 目的地情報の取得(順番通りに取得できるわけではない)
+        targets = GameObject.FindGameObjectsWithTag("Pillar");
+
+        // 最初の目的地を設定
+        agent.destination = targets[nextIndex].transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        // 衝突対象が目的地のピラーだったら次の目的地を設定する
+        if (collision.gameObject.tag == "Pillar")
+        {
+
+            // 次の目的地のインデックスにする
+            if (nextIndex < targets.Length - 1)
+            {
+                nextIndex++;
+            }
+            else
+            {
+                nextIndex = 0;
+            }
+
+            // インデックスに応じた目的地を設定する
+            agent.destination = targets[nextIndex].transform.position;
+        }
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey("up"))
+		{
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("down"))
+		{
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("right"))
+		{
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("left"))
+		{
+			transform.position -= transform.right * speed * Time.deltaTime;
+		}
+	}
+}
