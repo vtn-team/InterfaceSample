81c4cc659908e32817e366ab780da40d255ae6eb
DESKTOP-5AK3LVU\vantan <traitor10242526@gmail.com>
2021-06-05 14:10:59 +0900

    Enemy追加してtryまでやった

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        // Playerにあたったら消える
+        if (other.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
