04415dabb8ea9cf2f8279cbd9052da71c90e2f4d
k.mitarai <kazmit299^gmail.com>
2021-06-19 07:18:05 +0900

    mustの解答例

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ISave
+{
+    bool _isDead = false;
+
+    public virtual void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+
+    public virtual void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+
+    private void Update()
+    {
+        //将来的にはGameManagerに消してもらう
+        if (_isDead)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //将来的にはGameManagerに消してもらう
+            _isDead = true;
+        }
+    }
+}
