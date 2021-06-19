8963328a6b8ccdeef5a72771ded08f200a8e34d6
SAITO TAKUMI <nekotami1234@gmail.com>
2021-06-19 18:15:56 +0900

    Mastまで完成

---
+++ Assets/Scripts/Broken.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Broken : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject, 0.2f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ISave
+{
+   public virtual void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+
+    public virtual void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
