e878bd35ff36bdaaabf23f1dfe8f662d1ac9d28d
SetsunaTsuyuri <preciousprite@gmail.com>
2021-11-17 11:51:03 +0900

    must and try complete

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Enemy : FieldObject
+{
+    /// <summary>
+    /// コライダー無効化時間
+    /// </summary>
+    [SerializeField]
+    float disableCollisionDuration = 0.1f;
+
+    protected virtual void Start()
+    {
+        StartCoroutine(EnableCollider());
+    }
+
+    /// <summary>
+    /// コライダーを有効化する
+    /// </summary>
+    public IEnumerator EnableCollider()
+    {
+        yield return new WaitForSeconds(disableCollisionDuration);
+        GetComponent<Collider>().enabled = true;
+    }
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            Destroy(gameObject);
+            Debug.Log($"{name}はプレイヤーに触れ消えた");
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,4 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour, ISave
-{
-    public virtual void Save(SaveData.ObjectData data)
-    {
-        data.PrefabName = "Player";
-        data.Position = this.transform.position;
-    }
-
-    public virtual void Load(SaveData.ObjectData data)
-    {
-        this.transform.position = data.Position;
-    }
-}
+public class Player : FieldObject { }
