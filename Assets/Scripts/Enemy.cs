d7a276813bfcd189113326725764c73fcc033566
DESKTOP-0PML5RT\hidak <hidakaryo115274@gmail.com>
2021-11-17 13:00:21 +0900

    Mustまで回答

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, ISave
+{
+    public virtual void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+
+    public virtual void Load(SaveData.ObjectData data)
+    {
+        this.transform.position = data.Position;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
