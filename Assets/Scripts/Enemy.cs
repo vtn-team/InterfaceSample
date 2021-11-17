3588728a471db5d840c4bc01b0143350dc54f179
pb8vvs38zx <pb8vvs38zx@privaterelay.appleid.com>
2021-11-17 13:18:24 +0900

    Mustクリア、Tryに挑戦

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : FieldObject
+{
+    [SerializeField]
+    private GameObject player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == player.tag)
+        {
+            Debug.Log(player.name + "に当たった");
+            this.gameObject.SetActive(false);
+        }
+    }
+}
