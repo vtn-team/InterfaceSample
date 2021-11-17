8121cc386ee4d52746b722f78a317d5dae9c5b60
jackblack0094 <syomzn10@gmail.com>
2021-11-17 11:20:30 +0900

    Tryまでやった、自分が動かしやすいように少し変更

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : FieldObject, ISave
+{
+    public override void Save(SaveData.ObjectData data)
+    {
+        data.PrefabName = "Enemy";
+        data.Position = this.transform.position;
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("敵にぶつかった");
+            Destroy(this.gameObject);
+        }
+
+    }
+}
